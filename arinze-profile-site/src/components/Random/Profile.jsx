152efe71ce60bd8b1da00f59f965125f2d4ce992
Darlington Arinze <darlingtonarinze1995@gmail.com>
2023-02-28 23:57:08 +0100

    updated footer and profile section, with navbar

--- arinze-profile-site/src/components/Random/Profile.jsx
+++ arinze-profile-site/src/components/Random/Profile.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import productArr from "./profileArray";
 import "./Profile.scss";
+import productArr from "./profileArray";
 
 const Profile = () => {
-	// const =
+	const [showMore, setShowMore] = React.useState(true);
+
 	return (
 		<div className="prfle">
 			<h2>My Work</h2>
@@ -14,17 +15,19 @@ const Profile = () => {
 							<img src={item.img} alt={item.alt} className="scrnShot" />
 							<div className="prjtcrdTxt">
 								<h3>{item.h3}</h3>
-								<p>{item.p}</p>
+								<p onClick={() => setShowMore(!showMore)}>
+									{item.p.length > 200 && showMore ? `${item.p.slice(0, 181)}...` : item.p}
+								</p>
 								<div className="iconHldr">
 									<a href={item.github}>
 										<div className="box">
-											<i class="bx bxl-github"></i>
+											<i className="bx bxl-github"></i>
 											<span>Github</span>
 										</div>
 									</a>
-									<a href={item.site}>
+									<a href={item.site} target="_blank">
 										<div className="box">
-											<i class="bx bx-globe"></i>
+											<i className="bx bx-globe"></i>
 											<span>Visit Site</span>
 										</div>
 									</a>
