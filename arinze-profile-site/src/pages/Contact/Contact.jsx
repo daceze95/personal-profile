61cc03494eb04c0c6038dfdc20c34c884cec46da
Darlington Arinze <69388874+daceze95@users.noreply.github.com>
2023-03-09 13:22:06 +0100

    Update Contact.jsx

--- arinze-profile-site/src/pages/Contact/Contact.jsx
+++ arinze-profile-site/src/pages/Contact/Contact.jsx
@@ -21,7 +21,25 @@ const Contact = () => {
 				</div>
 			</div>
 			<div className="cnctTxtCtn">
-        <div className="cnctTxt">Coming soon...</div>
+		    <div className="container cnctTxt">
+      <h1>FormSubmit Demo</h1>
+      <form target="_blank" action="https://formsubmit.co/darlingtonarinze1995@gmail.com" method="POST">
+        <div className="form-group">
+          <div className="form-row">
+            <div className="col">
+              <input type="text" name="name" className="form-control" placeholder="Full Name" required />
+            </div>
+            <div className="col">
+              <input type="email" name="email" className="form-control" placeholder="Email Address" required />
+            </div>
+          </div>
+        </div>
+        <div className="form-group">
+          <textarea placeholder="Your Message" className="form-control" name="message" rows="8" required></textarea>
+        </div>
+        <button type="submit" className="btn btn-lg btn-primary btn-block">Submit Form</button>
+      </form>
+    </div>
       </div>
       <Footer/>
 		</div>
